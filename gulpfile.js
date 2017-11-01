8f42d17bca68ddf936f2b590f98f04a689eda6c4
Ramon Blanquer <blanquer.ramon@gmail.com>
2017-11-01 22:59:58 +0000

    Stylus working.

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,26 @@ var gulp = require('gulp');
 var babelify = require('babelify');
 var browserify = require('browserify');
 var buffer = require('vinyl-buffer');
+var concat = require('gulp-concat');
 var del = require('del');
 var es6ify = require('es6ify');
 var reactify = require('reactify');
 var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
 var sourcemaps = require('gulp-sourcemaps');
+var stylus = require('gulp-stylus');
 
 const vendors = ['react'];
+
+// Compile stylus into css
+gulp.task('styles', function() {
+  return gulp.src('frontend/*.styl')
+    .pipe(concat('styles.css'))
+    .pipe(sourcemaps.init())
+    .pipe(stylus())
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('public/css'));
+});
 
 // Build just vendor code so we don't have to rebuild all on app changes.
 gulp.task('vendor', function() {
@@ -25,7 +37,7 @@ gulp.task('vendor', function() {
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('public/js'));
-})
+});
 
 // Build the application code.
 gulp.task('build', function() {
@@ -48,10 +60,11 @@ gulp.task('build', function() {
 
 // Cleanup
 gulp.task('clean', function() {
-  return del(['public/js/*']);
-})
+  return del(['public/js/*', 'public/css/*']);
+});
 
 // Entry point
-gulp.task('default', ['build'], function() {
-  gulp.watch('frontend/*.jsx', ['build'])
+gulp.task('default', ['styles', 'build'], function() {
+  gulp.watch('frontend/*.jsx', ['build']);
+  gulp.watch('frontend/*.styl', ['styles'])
 });
