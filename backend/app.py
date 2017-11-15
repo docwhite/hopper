a54cf893484da1849d4500b51cd65fe744f30cf2
Ramon Blanquer <blanquer.ramon@gmail.com>
2017-11-15 18:26:38 +0000

    Put generator into a separate module.

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
 import os
-import random
+import sys
 
 from flask import Flask, render_template, jsonify
-from random_words import RandomNicknames, RandomWords, RandomEmails
 
-PRODUCTION = os.getenv('PRODUCTION', 1)
+from generator import generator
+
+PRODUCTION = bool(int(os.getenv("PRODUCTION", "1")))
 
 if PRODUCTION:
     STATIC = os.path.dirname(__file__)
@@ -13,21 +14,11 @@ else:
         os.path.join(os.path.dirname(__file__), '..', 'public')
     )
 
+print PRODUCTION
+print STATIC
+
 app = Flask(__name__)
 app._static_folder = STATIC
-
-def generateData():
-    result = []
-    for i in xrange(100):
-        gender = 'f' if random.randint(0, 1) == 0 else 'm'
-        result.append({
-            'id': i,
-            'name': RandomNicknames().random_nick(gender=gender),
-            'age': random.randint(0, 90),
-            'hobby': RandomWords().random_word()
-        })
-
-    return result
 
 @app.route('/')
 def index():
@@ -35,7 +26,7 @@ def index():
 
 @app.route('/data')
 def data():
-    result = generateData()
+    result = generator.generate()
     return jsonify(result)
 
 if __name__ == '__main__':
---
+++ backend/generator/generator.py
@@ -0,0 +1,30 @@
+"""Place all the data you want to present to the web frontend in this file, the
+generate() function has to return a list of python dictionaries of the
+following form:
+
+    [
+        {'id': 0, 'name': 'Ramon', 'age': 22, 'hobby': 'coding'},
+        {'id': 1, 'name': 'Neus', 'age': 17, 'hobby': 'cooking'},
+        ...
+    ]
+
+"""
+
+import random
+
+from random_words import RandomNicknames, RandomWords
+
+def generate():
+    result = []
+    for i in xrange(100):
+        gender = 'f' if random.randint(0, 1) == 0 else 'm'
+        result.append({
+            'id': i,
+            'name': RandomNicknames().random_nick(gender=gender),
+            'age': random.randint(0, 90),
+            'hobby': RandomWords().random_word()
+        })
+
+    return result
+
+
